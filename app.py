2b2533853d5fb783efc541d0e7db7a3264df9fd4
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-18 23:16:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #importing streamlit library
 
 import streamlit as st
+from datetime import date
 
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-#writing simple text 
+st.title('Stock Forecast App')
 
-st.text("Hello world!")
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-st.text("My name is Dominik and this website finally works <3")
-
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
