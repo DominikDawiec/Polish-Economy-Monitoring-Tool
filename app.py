c802fd7576d8ba6711bc8d099480a271fda4fff8
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 12:14:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,56 +4,22 @@ from datetime import date
 import yfinance as yf
 from plotly import graph_objs as go
 
-class Company:
-    def __init__(self, ticker):
-        price_df = si.get_data(ticker, dt.datetime.now()-dt.timedelta(days=2*365), dt.datetime.date(dt.datetime.now()))
-        overview_df = si.get_stats(ticker)
-        overview_df = overview_df.set_index('Attribute')
-        overview_dict = si.get_quote_table(ticker)
-        income_statement = si.get_income_statement(ticker)
-        balance_sheet = si.get_balance_sheet(ticker)
-        cash_flows = si.get_cash_flow(ticker)
-        
-        
-        
-self.year_end = overview_df.loc['Fiscal Year Ends'][0]
-self.market_cap = get_integer(overview_dict['Market Cap'])
-self.market_cap_cs = '{:,d}'.format(int(self.market_cap))
-self.prices = price_df['adjclose']
+# Here you can use markdown language to make your app prettier
+st.write("""
+# Financial App
+""")
 
-self.sales = income_statement.loc['totalRevenue'][0]
-self.gross_profit = income_statement.loc['grossProfit'][0]
-self.ebit = income_statement.loc['ebit'][0]
-self.interest = - income_statement.loc['interestExpense'][0]
-self.net_profit = income_statement.loc['netIncome'][0]
+# We will use Amazon stocks
+stock = 'AMZN'
 
-def get_overview(self):
-        self.price_earnings_ratio = self.market_cap/self.net_profit
-        self.ev_sales_ratio = self.ev/self.sales
-        self.overview_dict = {
-            'Values' : [self.ev_cs, self.market_cap_cs, self.ev_sales_ratio, self.price_earnings_ratio]
-            }
+# Get stock data
+get_stock_data = yf.Ticker(stock)
 
-def get_profit_margins(self):
-        self.gross_margin = self.gross_profit/self.sales
-        self.operating_margin = self.ebit/self.sales
-        self.net_margin = self.net_profit/self.sales
-        self.profit_margin_dict = {
-            'Values' : [self.gross_margin, self.operating_margin, self.net_margin]
-            }
-        
-        
-st.title('Financial Dashboard')
-ticker_input = st.text_input('Please enter your company ticker:')
-search_button = st.button('Search')
+# Set the time line of your data
+ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
 
-if search_button:
-    company = Company(ticker_input)
-    company.get_overview()
-    company.get_profit_margins()
-    company.get_liquidity_ratios()
-    company.get_leverage_ratios()
-    company.get_efficiency_ratios()
-    
-    
-    
+# Show your data in line chart
+st.line_chart(ticket_df.Close)
+st.line_chart(ticket_df.Volume)
+
+
