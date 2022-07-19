a1cc486230542197d1211ef342ff4cff8079c888
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 12:29:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import streamlit as st
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
 st.title('Polish Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
 
 
 
