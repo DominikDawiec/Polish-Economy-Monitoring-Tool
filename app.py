c68bc2bd796bfd607afd987db182a599e5d1a0d5
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 12:13:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,67 @@ from datetime import date
 import yfinance as yf
 from plotly import graph_objs as go
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+class Company:
+    def __init__(self, ticker):
+        price_df = si.get_data(ticker, dt.datetime.now()-dt.timedelta(days=2*365), dt.datetime.date(dt.datetime.now()))
+        overview_df = si.get_stats(ticker)
+        overview_df = overview_df.set_index('Attribute')
+        overview_dict = si.get_quote_table(ticker)
+        income_statement = si.get_income_statement(ticker)
+        balance_sheet = si.get_balance_sheet(ticker)
+        cash_flows = si.get_cash_flow(ticker)
+        
+        
+        
+self.year_end = overview_df.loc['Fiscal Year Ends'][0]
+self.market_cap = get_integer(overview_dict['Market Cap'])
+self.market_cap_cs = '{:,d}'.format(int(self.market_cap))
+self.prices = price_df['adjclose']
 
-st.title('Polish Stock Forecast App')
+self.sales = income_statement.loc['totalRevenue'][0]
+self.gross_profit = income_statement.loc['grossProfit'][0]
+self.ebit = income_statement.loc['ebit'][0]
+self.interest = - income_statement.loc['interestExpense'][0]
+self.net_profit = income_statement.loc['netIncome'][0]
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+def get_overview(self):
+        self.price_earnings_ratio = self.market_cap/self.net_profit
+        self.ev_sales_ratio = self.ev/self.sales
+        self.overview_dict = {
+            'Values' : [self.ev_cs, self.market_cap_cs, self.ev_sales_ratio, self.price_earnings_ratio]
+            }
 
-# n_years = st.slider('Years of prediction:', 1, 4)
-# period = n_years * 365
+def get_profit_margins(self):
+        self.gross_margin = self.gross_profit/self.sales
+        self.operating_margin = self.ebit/self.sales
+        self.net_margin = self.net_profit/self.sales
+        self.profit_margin_dict = {
+            'Values' : [self.gross_margin, self.operating_margin, self.net_margin]
+            }
+        
+        
+st.title('Financial Dashboard')
+ticker_input = st.text_input('Please enter your company ticker:')
+search_button = st.button('Search')
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
+if search_button:
+    company = Company(ticker_input)
+    company.get_overview()
+    company.get_profit_margins()
+    company.get_liquidity_ratios()
+    company.get_leverage_ratios()
+    company.get_efficiency_ratios()
+    
+    
+    
+st.header('Company overview')
+    overview_index = ['Enterprise value', 'Market cap', 'EV/sales ratio', 'P/E ratio']
+    overview_df = pd.DataFrame(company.overview_dict, index = overview_index)
+    st.line_chart(company.prices)
+    st.table(overview_df)
 
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
-
-st.subheader('Raw data')
-st.write(data.tail())
+    with st.beta_expander('Profit margins (as of {})'.format(company.year_end)):
+        profit_margin_index = ['Gross margin', 'Operating margin', 'Net margin']
+        profit_margin_df = pd.DataFrame(company.profit_margin_dict, index = profit_margin_index)
+        st.table(profit_margin_df)
+        st.bar_chart(profit_margin_df)
