fd46885ca3ea7ee4bd04673fd8faed3bd0855ee5
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 12:45:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,220 +1,222 @@
-import numpy as np
-import pandas as pd
-import yfinance as yf
-import matplotlib.pyplot as plt
+import functools
+from pathlib import Path
+
 import streamlit as st
-
-
-
-#### Getting GDP growth rate data by quarter
-
-gdp_gr = pd.read_html('https://www.multpl.com/us-gdp-growth-rate/table/by-quarter', match='Value')[0]
-
-gdp_gr['Date'] = pd.to_datetime(gdp_gr['Date'])
-
-gdp_gr.rename(columns={'Value Value': 'Value'}, inplace=True) 
-
-gdp_gr['Value'] = gdp_gr['Value'].str.strip('%')
-
-gdp_gr['Value'] = pd.to_numeric(gdp_gr['Value'])
-
-gdp_gr.set_index('Date', inplace=True)
-
-gdp_gr = gdp_gr.head(10)
-
-#### Getting US unemployment data by month
-
-unemployment = pd.read_html('https://www.multpl.com/unemployment/table/by-month', match='Rate')[0]
-
-unemployment['Date'] = pd.to_datetime(unemployment['Date'])
-
-unemployment.rename(columns={'Rate Value': 'Value'}, inplace=True)
-
-unemployment['Value'] = unemployment['Value'].str.strip('%')
-
-unemployment['Value'] = pd.to_numeric(unemployment['Value'])
-
-unemployment.set_index('Date', inplace=True)
-
-unemployment = unemployment.head(350)
-
-#### Getting US inflation rate monthly data by month
-
-inflation = pd.read_html('https://www.multpl.com/inflation/table/by-month', match='Value')[0]
-
-inflation['Date'] = pd.to_datetime(inflation['Date'])
-
-inflation.rename(columns={'Value Value': 'Value'}, inplace=True) 
-
-inflation['Value'] = inflation['Value'].str.strip('%')
-
-inflation['Value'] = pd.to_numeric(inflation['Value'])
-
-inflation.set_index('Date', inplace=True)
-
-inflation = inflation.head(50)
-
-## Getting US Retail sales monthly data 
-
-retail = pd.read_html('https://www.multpl.com/us-retail-sales-growth/table/by-month', match='Value')[0]
-
-retail['Date'] = pd.to_datetime(retail['Date'])
-
-retail.rename(columns={'Value Value': 'Value'}, inplace=True) 
-
-retail['Value'] = retail['Value'].str.strip('%')
-
-retail['Value'] = pd.to_numeric(retail['Value'])
-
-retail.set_index('Date', inplace=True)
-
-retail = retail.head(50)
-
-### Getting Shiller P/E 
-
-peshiller = pd.read_html('https://www.multpl.com/shiller-pe/table/by-month', match='Value')[0]
-
-peshiller['Date'] = pd.to_datetime(peshiller['Date'])
-
-peshiller.rename(columns={'Value Value': 'Value'}, inplace=True) 
-
-peshiller['Value'] = pd.to_numeric(peshiller['Value'])
-
-peshiller.set_index('Date', inplace=True)
-
-peshiller = peshiller.head(400)
-
-
-
-#### Getting 10Y Treasury yield 
-
-treasury = pd.read_html('https://www.multpl.com/10-year-treasury-rate/table/by-month', match='Value')[0]
-
-treasury['Date'] = pd.to_datetime(treasury['Date'])
-
-treasury.rename(columns={'Value Value': 'Value'}, inplace=True) 
-
-treasury['Value'] = treasury['Value'].str.strip('%')
-
-treasury['Value'] = pd.to_numeric(treasury['Value'])
-
-treasury.set_index('Date', inplace=True)
-
-treasury = treasury.head(20)
-
-
-#### Getting VIX Index daily price data
-
-vix = yf.Ticker("^VIX")
-
-vixIndex = vix.history(start='2021-01-01', interval='1d')
-
-vixclose = vixIndex['Close']
-
-
-### Getting Brent oil price daily data 
-
-brent = yf.Ticker("BZ=F")
-
-brentIndex = brent.history(start='2021-01-01')
-
-brentclose = brentIndex['Close']
-
-##### Building the streamlit APP
-
-st.set_page_config(page_title='Key Economic Indicators Dashboard', layout='wide')
-
-header= st.beta_container()
-
-with header:
-    st.header('Key Economic Indicators Dashboard')
-    st.subheader('The dashboard consists of 7 key indicators to analyse the economic and financial context: Gdp growth, unemployment rate, inflation, Us retail, P/E Shiller, Treasury yield, Vix volatility index and brent oil price')
-
-### First row of indicators: GDP, unemployment, inflation 
-
-gdp, unchart, inflchart = st.beta_columns(3)
-
-plt.style.use('ggplot')
-
-with gdp:
-    st.header('   GDP growth rate data by quarter')
-    fig1, ax = plt.subplots()
-    ax.set_xlabel('Date', labelpad=3.5)
-    ax.set_ylabel('Value in %')
-    plt.xticks(rotation=-19)
-    ax.plot(gdp_gr, marker='.', markersize=10)
-    st.pyplot(fig1)
-
-with unchart:
-    st.header('   US unemployment data by month')
-    fig2, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value in %')
-    ax.plot(unemployment)
-    st.pyplot(fig2)
-
-with inflchart:
-    st.header('  US inflation rate monthly data by month')
-    fig3, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value in %')
-    plt.xticks(rotation=-19)
-    ax.plot(inflation, marker='.', markersize=10)
-    st.pyplot(fig3)
-
-st.markdown('<hr>', unsafe_allow_html=True)
-
-### Second row of indicators: US Retail sales monthly data, Shiller P/E, 10Y Treasury yield
-
-retus, shiller, treas = st.beta_columns(3)
-
-with retus:
-    st.header('US retail monthly data')
-    fig4, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value')
-    plt.xticks(rotation=-19)
-    ax.plot(retail, marker='.', markersize=10)
-    st.pyplot(fig4)
-
-with shiller:
-    st.header('P/E Shiller - S&P 500')
-    fig5, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value')
-    ax.plot(peshiller, marker='.', markersize=7)
-    st.pyplot(fig5)
-
-with treas:
-    st.header(' 10y Treasury Yield')
-    fig6, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Yield in %')
-    plt.xticks(rotation=-19)
-    ax.plot(treasury, marker='.', markersize=10)
-    st.pyplot(fig6)
-
-st.markdown('<hr>', unsafe_allow_html=True)
-
-### Third row of indicators: VIX Index, Brent oil
-
-vol, oil = st.beta_columns(2)
-
-with vol:
-    st.header('Vix Volatility Index')
-    fig7, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Value')
-    ax.plot(vixclose)
-    st.pyplot(fig7)
-
-with oil:
-    st.header(' Brent Oil Price Index')
-    fig8, ax = plt.subplots()
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Price')
-    ax.plot(brentclose)
-    st.pyplot(fig8)
-
-
-
+from st_aggrid import AgGrid
+from st_aggrid.shared import JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import pandas as pd
+import plotly.express as px
+
+chart = functools.partial(st.plotly_chart, use_container_width=True)
+COMMON_ARGS = {
+    "color": "symbol",
+    "color_discrete_sequence": px.colors.sequential.Greens,
+    "hover_data": [
+        "account_name",
+        "percent_of_account",
+        "quantity",
+        "total_gain_loss_dollar",
+        "total_gain_loss_percent",
+    ],
+}
+
+
+@st.experimental_memo
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Take Raw Fidelity Dataframe and return usable dataframe.
+    - snake_case headers
+    - Include 401k by filling na type
+    - Drop Cash accounts and misc text
+    - Clean $ and % signs from values and convert to floats
+
+    Args:
+        df (pd.DataFrame): Raw fidelity csv data
+
+    Returns:
+        pd.DataFrame: cleaned dataframe with features above
+    """
+    df = df.copy()
+    df.columns = df.columns.str.lower().str.replace(" ", "_", regex=False).str.replace("/", "_", regex=False)
+
+    df.type = df.type.fillna("unknown")
+    df = df.dropna()
+
+    price_index = df.columns.get_loc("last_price")
+    cost_basis_index = df.columns.get_loc("cost_basis_per_share")
+    df[df.columns[price_index : cost_basis_index + 1]] = df[
+        df.columns[price_index : cost_basis_index + 1]
+    ].transform(lambda s: s.str.replace("$", "", regex=False).str.replace("%", "", regex=False).astype(float))
+
+    quantity_index = df.columns.get_loc("quantity")
+    most_relevant_columns = df.columns[quantity_index : cost_basis_index + 1]
+    first_columns = df.columns[0:quantity_index]
+    last_columns = df.columns[cost_basis_index + 1 :]
+    df = df[[*most_relevant_columns, *first_columns, *last_columns]]
+    return df
+
+
+@st.experimental_memo
+def filter_data(
+    df: pd.DataFrame, account_selections: list[str], symbol_selections: list[str]
+) -> pd.DataFrame:
+    """
+    Returns Dataframe with only accounts and symbols selected
+
+    Args:
+        df (pd.DataFrame): clean fidelity csv data, including account_name and symbol columns
+        account_selections (list[str]): list of account names to include
+        symbol_selections (list[str]): list of symbols to include
+
+    Returns:
+        pd.DataFrame: data only for the given accounts and symbols
+    """
+    df = df.copy()
+    df = df[
+        df.account_name.isin(account_selections) & df.symbol.isin(symbol_selections)
+    ]
+
+    return df
+
+
+def main() -> None:
+    st.header("Fidelity Account Overview :moneybag: :dollar: :bar_chart:")
+
+    with st.expander("How to Use This"):
+        st.write(Path("README.md").read_text())
+
+    st.subheader("Upload your CSV from Fidelity")
+    uploaded_data = st.file_uploader(
+        "Drag and Drop or Click to Upload", type=".csv", accept_multiple_files=False
+    )
+
+    if uploaded_data is None:
+        st.info("Using example data. Upload a file above to use your own data!")
+        uploaded_data = open("example.csv", "r")
+    else:
+        st.success("Uploaded your file!")
+
+    df = pd.read_csv(uploaded_data)
+    with st.expander("Raw Dataframe"):
+        st.write(df)
+
+    df = clean_data(df)
+    with st.expander("Cleaned Data"):
+        st.write(df)
+
+    st.sidebar.subheader("Filter Displayed Accounts")
+
+    accounts = list(df.account_name.unique())
+    account_selections = st.sidebar.multiselect(
+        "Select Accounts to View", options=accounts, default=accounts
+    )
+    st.sidebar.subheader("Filter Displayed Tickers")
+
+    symbols = list(df.loc[df.account_name.isin(account_selections), "symbol"].unique())
+    symbol_selections = st.sidebar.multiselect(
+        "Select Ticker Symbols to View", options=symbols, default=symbols
+    )
+
+    df = filter_data(df, account_selections, symbol_selections)
+    st.subheader("Selected Account and Ticker Data")
+    cellsytle_jscode = JsCode(
+        """
+    function(params) {
+        if (params.value > 0) {
+            return {
+                'color': 'white',
+                'backgroundColor': 'forestgreen'
+            }
+        } else if (params.value < 0) {
+            return {
+                'color': 'white',
+                'backgroundColor': 'crimson'
+            }
+        } else {
+            return {
+                'color': 'white',
+                'backgroundColor': 'slategray'
+            }
+        }
+    };
+    """
+    )
+
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_columns(
+        (
+            "last_price_change",
+            "total_gain_loss_dollar",
+            "total_gain_loss_percent",
+            "today's_gain_loss_dollar",
+            "today's_gain_loss_percent",
+        ),
+        cellStyle=cellsytle_jscode,
+    )
+    gb.configure_pagination()
+    gb.configure_columns(("account_name", "symbol"), pinned=True)
+    gridOptions = gb.build()
+
+    AgGrid(df, gridOptions=gridOptions, allow_unsafe_jscode=True)
+
+    def draw_bar(y_val: str) -> None:
+        fig = px.bar(df, y=y_val, x="symbol", **COMMON_ARGS)
+        fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
+        chart(fig)
+
+    account_plural = "s" if len(account_selections) > 1 else ""
+    st.subheader(f"Value of Account{account_plural}")
+    totals = df.groupby("account_name", as_index=False).sum()
+    if len(account_selections) > 1:
+        st.metric(
+            "Total of All Accounts",
+            f"${totals.current_value.sum():.2f}",
+            f"{totals.total_gain_loss_dollar.sum():.2f}",
+        )
+    for column, row in zip(st.columns(len(totals)), totals.itertuples()):
+        column.metric(
+            row.account_name,
+            f"${row.current_value:.2f}",
+            f"{row.total_gain_loss_dollar:.2f}",
+        )
+
+    fig = px.bar(
+        totals,
+        y="account_name",
+        x="current_value",
+        color="account_name",
+        color_discrete_sequence=px.colors.sequential.Greens,
+    )
+    fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
+    chart(fig)
+
+    st.subheader("Value of each Symbol")
+    draw_bar("current_value")
+
+    st.subheader("Value of each Symbol per Account")
+    fig = px.sunburst(
+        df, path=["account_name", "symbol"], values="current_value", **COMMON_ARGS
+    )
+    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    chart(fig)
+
+    st.subheader("Value of each Symbol")
+    fig = px.pie(df, values="current_value", names="symbol", **COMMON_ARGS)
+    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    chart(fig)
+
+    st.subheader("Total Value gained each Symbol")
+    draw_bar("total_gain_loss_dollar")
+    st.subheader("Total Percent Value gained each Symbol")
+    draw_bar("total_gain_loss_percent")
+
+
+if __name__ == "__main__":
+    st.set_page_config(
+        "Fidelity Account View by Gerard Bentley",
+        "📊",
+        initial_sidebar_state="expanded",
+        layout="wide",
+    )
+    main()
