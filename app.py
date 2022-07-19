7f1679af4a77e962073a992c046aed09d97850a7
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 11:58:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,24 +41,3 @@ def plot_raw_data():
     st.plotly_chart(fig)
     
 plot_raw_data()
-
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-    
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
