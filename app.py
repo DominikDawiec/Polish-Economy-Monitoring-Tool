b4ec46ec5bcb88fe60615f126953a1340cc75408
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 11:44:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,21 @@
-import streamlit as st
-from datetime import date
-
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
+import streamlit as st
+import pandas as pd
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+# Here you can use markdown language to make your app prettier
+st.write("""
+# Financial App
+""")
 
-st.title('Stock Forecast App')
+# We will use Amazon stocks
+stock = 'AMZN'
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+# Get stock data
+get_stock_data = yf.Ticker(stock)
 
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+# Set the time line of your data
+ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
 
-
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-    
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
-
-st.subheader('Raw data')
-st.write(data.tail())
-
-# Plot raw data
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
-    
-plot_raw_data()
-
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-    
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+# Show your data in line chart
+st.line_chart(ticket_df.Close)
+st.line_chart(ticket_df.Volume)
