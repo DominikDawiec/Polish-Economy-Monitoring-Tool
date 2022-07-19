a0887244ea5103273a219d92be4c518b15c363ff
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-07-19 12:16:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,4 @@ stock = 'AMZN'
 # Get stock data
 get_stock_data = yf.Ticker(stock)
 
-# Set the time line of your data
-ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
-
-# Show your data in line chart
-st.line_chart(ticket_df.Close)
-st.line_chart(ticket_df.Volume)
-
-
+st.write(get_stock_data)
