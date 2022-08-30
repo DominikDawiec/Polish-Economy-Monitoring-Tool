fd05fb25591062727cc1df3bbab2eeedbba9117a
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-08-30 21:49:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,25 @@
-import functools
-from pathlib import Path
-
+import yfinance as yf
 import streamlit as st
-from st_aggrid import AgGrid
-from st_aggrid.shared import JsCode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-import pandas as pd
-import plotly.express as px
 
-chart = functools.partial(st.plotly_chart, use_container_width=True)
-COMMON_ARGS = {
-    "color": "symbol",
-    "color_discrete_sequence": px.colors.sequential.Greens,
-    "hover_data": [
-        "account_name",
-        "percent_of_account",
-        "quantity",
-        "total_gain_loss_dollar",
-        "total_gain_loss_percent",
-    ],
-}
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
 
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-@st.experimental_memo
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Take Raw Fidelity Dataframe and return usable dataframe.
-    - snake_case headers
-    - Include 401k by filling na type
-    - Drop Cash accounts and misc text
-    - Clean $ and % signs from values and convert to floats
-
-    Args:
-        df (pd.DataFrame): Raw fidelity csv data
-
-    Returns:
-        pd.DataFrame: cleaned dataframe with features above
-    """
-    df = df.copy()
-    df.columns = df.columns.str.lower().str.replace(" ", "_", regex=False).str.replace("/", "_", regex=False)
-
-    df.type = df.type.fillna("unknown")
-    df = df.dropna()
-
-    price_index = df.columns.get_loc("last_price")
-    cost_basis_index = df.columns.get_loc("cost_basis_per_share")
-    df[df.columns[price_index : cost_basis_index + 1]] = df[
-        df.columns[price_index : cost_basis_index + 1]
-    ].transform(lambda s: s.str.replace("$", "", regex=False).str.replace("%", "", regex=False).astype(float))
-
-    quantity_index = df.columns.get_loc("quantity")
-    most_relevant_columns = df.columns[quantity_index : cost_basis_index + 1]
-    first_columns = df.columns[0:quantity_index]
-    last_columns = df.columns[cost_basis_index + 1 :]
-    df = df[[*most_relevant_columns, *first_columns, *last_columns]]
-    return df
-
-
-@st.experimental_memo
-def filter_data(
-    df: pd.DataFrame, account_selections: list[str], symbol_selections: list[str]
-) -> pd.DataFrame:
-    """
-    Returns Dataframe with only accounts and symbols selected
-
-    Args:
-        df (pd.DataFrame): clean fidelity csv data, including account_name and symbol columns
-        account_selections (list[str]): list of account names to include
-        symbol_selections (list[str]): list of symbols to include
-
-    Returns:
-        pd.DataFrame: data only for the given accounts and symbols
-    """
-    df = df.copy()
-    df = df[
-        df.account_name.isin(account_selections) & df.symbol.isin(symbol_selections)
-    ]
-
-    return df
-
-
-st.header("Polish Stock Market Prediction :moneybag: :dollar: :bar_chart:")
-
-   
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
