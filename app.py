a3de00edb4227ad00d47a4895e74809e7713cba7
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-08-31 12:59:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,81 @@
 import streamlit as st
-import yfinance as finance
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.title('Stock forecast dashboard')
 
 
-def get_ticker(name):
-	company = finance.Ticker(name) # google
-	return company
+
+import datetime 
+
+def nearest_business_day(DATE: datetime.date):
+    """
+    Takes a date and transform it to the nearest business day
+    """
+    if DATE.weekday() == 5:
+        DATE = DATE - datetime.timedelta(days=1)
+
+    if DATE.weekday() == 6:
+        DATE = DATE + datetime.timedelta(days=1)
+    return DATE
+      
+      
+# ------ layout setting---------------------------
+window_selection_c = st.sidebar.container() # create an empty container in the sidebar
+window_selection_c.markdown("## Insights") # add a title to the sidebar container
+sub_columns = window_selection_c.columns(2) #Split the container into two columns for start and end date
+
+# ----------Time window selection-----------------
+YESTERDAY=datetime.date.today()-datetime.timedelta(days=1)
+YESTERDAY = nearest_business_day(YESTERDAY) #Round to business day
+
+DEFAULT_START=YESTERDAY - datetime.timedelta(days=700)
+DEFAULT_START = nearest_business_day(DEFAULT_START)
+
+START = sub_columns[0].date_input("From", value=DEFAULT_START, max_value=YESTERDAY - datetime.timedelta(days=1))
+END = sub_columns[1].date_input("To", value=YESTERDAY, max_value=YESTERDAY, min_value=START)
+
+START = nearest_business_day(START)
+END = nearest_business_day(END)
 
 
-# Project Details
-st.title("Build and Deploy Stock Market App Using Streamlit")
-st.header("A Basic Data Science Web Application")
-st.sidebar.header("Geeksforgeeks \n TrueGeeks")
 
-company1 = get_ticker("GOOGL")
-company2 = get_ticker("MSFT")
 
-# fetches the data: Open, Close, High, Low and Volume
-google = finance.download("GOOGL", start="2021-10-01", end="2021-10-01")
-microsoft = finance.download("MSFT", start="2021-10-01", end="2021-10-01")
+# ---------------stock selection------------------
+STOCKS = np.array([ "GOOG", "GME", "FB","AAPL",'TSLA'])  
+SYMB = window_selection_c.selectbox("select stock", STOCKS)
 
-# Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-data1 = company1.history(period="3mo")
-data2 = company2.history(period="3mo")
 
-# markdown syntax
-st.write("""
-### Google
-""")
 
-# detailed summary on Google
-st.write(company1.info['longBusinessSummary'])
-st.write(google)
+chart_width = st.expander(label="chart width").slider("", 1000, 2800, 1400) #arguments: label, min, default, max
 
-# plots the graph
-st.line_chart(data1.values)
 
-st.write("""
-### Microsoft
-""")
-st.write(company2.info['longBusinessSummary'], "\n", microsoft)
-st.line_chart(data2.values)
+
+ def show_delta(self):
+        """
+        Visualize a summary of the stock change over the specified time period
+        """
+        epsilon = 1e-6 
+        i = self.start
+        j = self.end
+        s = self.data.query("date==@i")['Close'].values[0]
+        e = self.data.query("date==@j")['Close'].values[0]
+
+        difference = round(e - s, 2) 
+        change = round(difference / (s + epsilon) * 100, 2) #Denominator is always >0
+        e = round(e, 2)
+        cols = st.columns(2)
+        (color, marker) = ("green", "+") if difference >= 0 else ("red", "-")
+
+        cols[0].markdown(
+            f"""<p style="font-size: 90%;margin-left:5px">{self.symbol} \t {e}</p>""",
+            unsafe_allow_html=True 
+        )
+        cols[1].markdown(
+            f"""<p style="color:{color};font-size:90%;margin-right:5px">{marker} \t {difference} {marker} {change} % </p>""",
+            unsafe_allow_html=True
+        )
+	
+	
+	
+change_c = st.sidebar.container()
+with change_c:
+    stock.show_delta()
