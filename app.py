e734ec8e236ab2879b40c0ced6330a33a400f371
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-08-31 15:14:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ st.header('This is a header')
 st.subheader('This is a subheader')
 st.caption('This is a string that explains something above.')
 
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+# st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
-st.metric(label="Gas price", value=4, delta=-0.5,
-     delta_color="inverse")
+
+3 st.metric(label="Gas price", value=4, delta=-0.5, delta_color="inverse")
 
 df = pd.read_csv('indexData.csv')
 st.dataframe(df)
@@ -28,8 +28,7 @@ option = st.selectbox(
 
 If option == 'X':
      st.header('X')
-     st.subheader('Description')
-     st.write('It is a sigmoid function with a characteristic "S"-shaped curve.')
+     
 
 
 
@@ -44,14 +43,3 @@ If option == 'X':
 # all_symbols = source.Index.unique()
 # symbols = st.selectbox("Choose stocks to visualize", all_symbols)
 
-
-
-
-st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Google!
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
