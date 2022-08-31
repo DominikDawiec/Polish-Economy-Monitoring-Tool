55b65ea56dca35f8cb65eea1891e40b22df8635c
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-08-31 12:50:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
-import yfinance as yf
+import fred
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Google!
-""")
-
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
+# Save your FRED API key.
+fred.key('8c3b945500069081b94040df2da12df7')
 
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+st.title('Polish Macroeconomy App')
+dataset = ('Inflacja', 'Stopa bezrobocia', 'Produkt PKB Brutto')
+option = st.selectbox('Wybierz interesujący Cię wskaźnik',dataset)
+
+
+df = fred.observations('CP0452PLM086NEST')
+Inflacja = pd.DataFrame.from_dict(df['observations'])
+Inflacja['date'] = pd.to_datetime(Inflacja['date'])
+Inflacja['value'] = pd.to_numeric(df1['value'],errors = 'coerce')
+
+
+
+import plotly.express as px
+import pandas as pd
+
+df1 = df1
+
+fig = px.line(df1, x='date', y='value', title='Time Series with Range Slider and Selectors')
+
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+fig.show()
