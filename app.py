70b2baf7ebf960eaaa30597a16d872035c79da5d
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-08-31 13:17:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,6 @@
-import yfinance as yf
-import streamlit as st
+#import streamlit as st 
 import pandas as pd
+import plotly.graph_objects as go
+#from streamlit_lottie 
+import json
 
-# Here you can use markdown language to make your app prettier
-st.write("""
-# Financial App
-""")
-
-list = ('1','2','3')
-
-option = st.selectbox(
-     'How would you like to be contacted?', list)
-
-if st.button('show indicator'):
-     choosen_indicator = get_indicators(option)
-
-
-# We will use Amazon stocks
-stock = 'AMZN'
-
-# Get stock data
-get_stock_data = yf.Ticker(stock)
-
-# Set the time line of your data
-ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
-
-# Show your data in line chart
-st.line_chart(ticket_df.Close)
-st.line_chart(ticket_df.Volume)
