2635c49846eaa2fb9797ac23c0cf78e4a4059bf5
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-01 16:08:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,17 +76,4 @@ Inflacja.to_csv('plik2.csv')
 
 df1 = Inflacja
 fig = px.line(df1, x='date', y='value', title='Time Series with Range Slider and Selectors')
-
-fig.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(count=1, label="YTD", step="year", stepmode="todate"),
-            dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")
-        ])
-    )
-)
 st.pyplot(fig)
