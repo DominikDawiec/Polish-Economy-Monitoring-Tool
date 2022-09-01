8f50afe227b62a38e5e9daec1f771a730bd59d2f
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-01 16:06:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,32 @@ arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
+
+
+
+import fred
+fred.key('8c3b945500069081b94040df2da12df7')
+df = fred.observations('CP0452PLM086NEST')
+Inflacja = pd.DataFrame.from_dict(df['observations'])
+Inflacja['date'] = pd.to_datetime(Inflacja['date'])
+Inflacja['value'] = pd.to_numeric(Inflacja['value'],errors = 'coerce')
+Inflacja.to_csv('plik2.csv')
+
+
+df1 = Inflacja
+fig = px.line(df1, x='date', y='value', title='Time Series with Range Slider and Selectors')
+
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+fig.show()
 st.pyplot(fig)
