028c5dd232710a133802bca407768b015553b24c
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-01 16:15:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,8 +81,36 @@ st.plotly_chart(fig, use_container_width=True)
 
 
 
+
+
+
 import plotly.graph_objects as go
 
-fig1 = go.Figure(data=[go.Table(df1)])
-st.plotly_chart(fig1, use_container_width=True)
+headerColor = 'grey'
+rowEvenColor = 'lightgrey'
+rowOddColor = 'white'
 
+fig = go.Figure(data=[go.Table(
+  header=dict(
+    values=['<b>EXPENSES</b>','<b>Q1</b>','<b>Q2</b>','<b>Q3</b>','<b>Q4</b>'],
+    line_color='darkslategray',
+    fill_color=headerColor,
+    align=['left','center'],
+    font=dict(color='white', size=12)
+  ),
+  cells=dict(
+    values=[
+      ['Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'],
+      [1200000, 20000, 80000, 2000, 12120000],
+      [1300000, 20000, 70000, 2000, 130902000],
+      [1300000, 20000, 120000, 2000, 131222000],
+      [1400000, 20000, 90000, 2000, 14102000]],
+    line_color='darkslategray',
+    # 2-D list of colors for alternating rows
+    fill_color = [[rowOddColor,rowEvenColor,rowOddColor, rowEvenColor,rowOddColor]*5],
+    align = ['left', 'center'],
+    font = dict(color = 'darkslategray', size = 11)
+    ))
+])
+
+st.plotly_chart(fig, use_container_width=True)
