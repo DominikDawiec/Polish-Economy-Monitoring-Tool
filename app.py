0d5884459e1fa196283cb2e9dfbd82eb6d899c21
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-02 15:38:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,3 +107,16 @@ testfunction(choose_category2)
 st.dataframe(dfz)
 
 subcategory(choose_category2)
+
+st.title("NEW START")
+
+categories3 = dfz.title.unique()
+choose_category3 = st.selectbox("Choose an economic indicator:", categories3)
+
+def subcategory5(x):
+  hjk = dfz.loc[dfz['title'] ==x
+  st.dataframe(hjk)
+                
+subcategory5(choose_category3)   
+
+
