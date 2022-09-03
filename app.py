61e017d8567a381cc231bd5d175d85823b296958
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-03 23:42:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,100 +7,21 @@ import plotly.graph_objects as go
 import fred
 from fredapi import Fred
 
+# setting the website
+st.set_page_config(
+     page_title="Polish Economic Indicators",
+     page_icon="📊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+ )
+
 # key
 fred.key('8c3b945500069081b94040df2da12df7')
 
-# page setup
-st.set_page_config(page_title="Polish Macroeconomic Indicators", page_icon="📊", initial_sidebar_state="expanded")
+# downloading list of avaiable variables concerning Poland
+all_variables = fred.category_series(32339)
+all_variables = pd.DataFrame.from_dict(all_variables['seriess'])
+all_variables['subtitle'] = all_variables['title'] + ", FREQUENCY.:" + all_variables['frequency'] + ', UNIT:' + all_variables['units'] + ', SEASONAL ADJUSTMENT:' + all_variables['seasonal_adjustment']
 
-# title
-st.title('Polish Macroeconomic Indicators')
-st.subheader('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-st.caption('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
-
-
-
-def main_plot(fname):
-  # downloading timeseries data from API
-  df = fred.observations(fname)
-  
-  # cleaning the dataset with timeseries
-  df = pd.DataFrame.from_dict(df['observations'])
-  df['date'] = pd.to_datetime(df['date'])
-  df['value'] = pd.to_numeric(df['value'],errors = 'coerce')
-  
-  # downloading detailed data from API
-  dfx = fred.search(fname)
-  
-  # cleaning detailed data from API
-  dfx = pd.DataFrame.from_dict(dfx['seriess'])
- 
-  # downloading data about source
-  dfa = fred.series(fname, release=True)
-  
-  # cleaning detailed about source
-  dfa = pd.DataFrame.from_dict(dfa['releases'])
- 
-  # creating plot with timeseries
-  fig = px.line(df, x='date', y='value', title= dfx['title'].iat[-1])
-  st.plotly_chart(fig, use_container_width=True)
-  
-  # creating KPIs
-  st.write('Observation Start:', dfx['observation_start'].iat[-1])
-  st.write('Observation End:', dfx['observation_end'].iat[-1])
-  st.write('Frequency:', dfx['frequency'].iat[-1])
-  st.write('Unit:', dfx['units'].iat[-1])
-  st.write('Seasonal Adjustment:', dfx['seasonal_adjustment'].iat[-1])
-  st.write('Last Updated:', dfx['last_updated'].iat[-1])
-  st.write('Source Link:', dfa['link'].iat[-1])
-
-  # Calculations for KPIs
-  last_value = df['value'].iat[-1]
-  penultimate_value = df['value'].iat[-2]
-  momdiff = ((last_value - penultimate_value)/penultimate_value)*100
-  
-  # creating KPIs
-  st.write('Last Value:', last_value)
-  st.write('Penultimate Value:', penultimate_value)
-  st.write('MoM Change:', momdiff, "%")
-
-  #st.dataframe(dfx)
-  #st.dataframe(df)
-
-dfz = fred.category_series(32339)
-dfz = pd.DataFrame.from_dict(dfz['seriess'])
-dfz['name'] = dfz['title'] + " (" + dfz['id'] + ")"
-dfz.sort_values(by='title')
-
-# categories = dfz.title.unique()
-# choose_category = st.selectbox("Choose an economic indicator:", categories)
-
-# main_plot(choose_category)
-
-
-# dfz.loc[dfz["name"] =="Harmonized Index of Consumer Prices: Overall Index Excluding Energy, Food, Alcohol, and Tobacco for Poland (00XEFDPLM086NEST)", ["id", "name", "notes"]]
-categories2 = dfz.title.unique()
-choose_category2 = st.selectbox("Choose an economic indicator:", categories2)
-
-
-def testfunction(x):
-  ghb = dfz.loc[dfz["title"] ==x, ["id", "name", "notes"]]
-  main_plot(ghb['id'].iat[-1])
-  st.write("notes:", ghb['notes'].iat[-1])
-
- 
-# testfunction(choose_category2)
-
-st.title("NEW START")
-
-sdffssf = dfz.title.unique()
-bruh = st.selectbox("Choose an economic indicator:", sdffssf)
-
-
-#def subcategory5(x):
- # hjk = dfz.loc[dfz['title'] ==x]
-  #st.dataframe(hjk)
-                
-#subcategory5(choose_category3)   
-
-
+# creating a list of variables to select 
+all_variables_to_select = all_variables.title.unique()
