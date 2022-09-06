58c39f8ea06cdce5ea4ce7779c77139a105a8fe7
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-06 21:38:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -189,18 +189,18 @@ def analitical_insights():
                
                fig.add_vrect(x0="2020-02-01", x1="2020-04-31", 
                              annotation_text="Covid-19 Crisis", annotation_position="top left",
-                             annotation=dict(font_size=12, font_family="Times New Roman"),
-                             fillcolor="red", opacity=1, line_width=0.5)
+                             annotation=dict(font_size=20, font_family="Times New Roman"),
+                             fillcolor="blue", opacity=0.25, line_width=0)
                
                fig.add_vrect(x0="2007-12-01", x1="2009-06-31", 
                              annotation_text="Global Crisis", annotation_position="top left",
-                             annotation=dict(font_size=12, font_family="Times New Roman"),
-                             fillcolor="red", opacity=0, line_width=5)
+                             annotation=dict(font_size=20, font_family="Times New Roman"),
+                             fillcolor="blue", opacity=0.25, line_width=0)
                
                fig.add_vrect(x0="2001-03-01", x1="2001-11-31", 
                              annotation_text="Internet Bubble Crisis", annotation_position="top left",
-                             annotation=dict(font_size=12, font_family="Times New Roman"),
-                             fillcolor="red", opacity=0.25, line_width=0)
+                             annotation=dict(font_size=20, font_family="Times New Roman"),
+                             fillcolor="blue", opacity=0.25, line_width=0)
                
                st.plotly_chart(fig, use_container_width=True)
                
