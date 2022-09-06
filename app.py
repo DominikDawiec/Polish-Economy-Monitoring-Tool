0f254e1dae9d31879b303f3d7aff80a5d0d067ad
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-06 20:55:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,126 @@ chosen_variable_name = st.selectbox('Please select an indicator', list_of_availa
 choose_variable(chosen_variable_name)
 variable_ID = choose_variable.ID
 
-st.write(variable_ID)
+
+# Defining functions
+# =====================================================================================================
+
+# Function to download data 
+def download_data(variable):
+    timeseries = fred.observations(variable)
+    timeseries = pd.DataFrame.from_dict(timeseries['observations'])
+    timeseries['date'] = pd.to_datetime(timeseries['date'])
+    timeseries['date'] = timeseries['date'].dt.date
+    timeseries['value'] = pd.to_numeric(timeseries['value'],errors = 'coerce')
+    timeseries = timeseries.drop(['realtime_start', 'realtime_end'], axis=1)
+    download_data.timeseries = timeseries
+    
+    info_1 = fred.search(variable)
+    info_1 = pd.DataFrame.from_dict(info_1['seriess'])
+    info_1.replace(r'\s+', np.nan, regex=True)
+    download_data.info_1 = info_1
+    
+    info_2 = fred.series(variable, release=True)
+    info_2 = pd.DataFrame.from_dict(info_2['releases'])
+    info_1.replace(r'\s+', np.nan, regex=True)
+    download_data.info_2 = info_2
+
+# Setting function Creating Plot
+def plot(variable):
+    with st.container():
+         tab1, tab2 = st.tabs(["Historical Chart 📈", "Historical Data 💾"])
+            
+            with tab1:
+                st.header("Historical Chart 📈")
+                
+                fig = px.line(timeseries, x='date', y="value")
+                fig.update_layout(
+                    yaxis_title='Value',
+                    xaxis_title='Date',
+                    title='To be updated xyz',
+                    hovermode="x")
+                
+                fig.update_xaxes(rangeslider_visible=True)
+                
+                fig.update_xaxes(
+                    rangeslider_visible=True,
+                    rangeselector=dict(
+                        buttons=list([
+                            dict(count=6, label="6m", step="month", stepmode="backward"),
+                            dict(count=1, label="1y", step="year", stepmode="backward"),
+                            dict(count=5, label="5y", step="year", stepmode="backward"),
+                            dict(count=10, label="10y", step="year", stepmode="backward"),
+                            dict(step="all")])))
+                
+                st.plotly_chart(fig, use_container_width=True)
+                
+            with tab2:
+                st.header("Historical Data 💾")
+                
+                fig = go.Figure(data=[go.Table(header=dict(values=['<b>DATE<b>', '<b>VALUE<b>'], 
+                                                           line_color='black',
+                                                           font=dict(color='white'),
+                                                           align=['left'],
+                                                           fill_color='#636EFA'),
+                                               cells=dict(values=[df['date'], df['value']], 
+                                                          font=dict(color='black'),
+                                                          align=['left'],
+                                                          line_color='black',
+                                                          fill_color='white'))])
+                st.plotly_chart(fig, use_container_width=True)
+    
+    with st.container():
+        st.header("KPIs 📟")
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Ultimate value", ultimate_value)
+        col2.metric("Preultimate value", preultimate_value)
+        col3.metric("Percentage change", percentage_change)
+        
+    with st.container():
+        st.header("Details 📇")
+        
+        values = [['title', 'observation_start', 'observation_end', 'frequency', 'units', 'seasonal_adjustment', 'last_updated', 'link', 'notes'], #1st col
+                  [info_1['title'].iat[-1],
+                   info_1['observation_start'].iat[-1],
+                   info_1['observation_end'].iat[-1],
+                   info_1['frequency'].iat[-1],
+                   info_1['units'].iat[-1],
+                   info_1['seasonal_adjustment'].iat[-1],
+                   info_1['last_updated'].iat[-1],
+                   info_2['link'].iat[-1],
+                   info_1['notes'].iat[-1]]]
+        fig = go.Figure(data=[go.Table(
+            columnorder = [1,2],
+            columnwidth = [100,450],
+            header = dict(
+                values = [['<b>CATEGORY</b>'],['<b>DESCRIPTION</b>']],
+                line_color='black',
+                fill_color='#636EFA',
+                align=['left'],
+                font=dict(color='white', size=12),
+                height=40),
+            cells=dict(
+            values=values,
+            line_color='black',
+            fill=dict(color=['white', 'white']),
+            align=['left'],
+            font=dict(color='black'),
+            font_size=12,
+            height=30))])
+        fig.update_layout(height=550)
+        st.plotly_chart(fig, use_container_width=True)
+
+
+download_data(variable_ID)
+
+# saving attributes outside functions
+timeseries = download_data.timeseries
+info_1 = download_data.info_1
+info_2 = download_data.info_2
+
+# creating values for KPIs
+ultimate_value = timeseries['value'].iat[-1]
+preultimate_value = timeseries['value'].iat[-2]
+percentage_change = ((ultimate_value - preultimate_value) / preultimate_value) * 100
+
+plot()
