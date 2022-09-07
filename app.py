7ebcafa1d09096169425eb1539a61e4aa3da9e48
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-07 13:48:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.set_page_config(
 # Main page
 st.title("📊 Polish Economy Monitoring Tool")
 st.caption("The application is designed to enable viewing and analyzing economic indicators for Poland in real time, without the need to manually update the database.")
-st.caption("In addition, the application has been enriched with a module for variable analysis, a module creating a forecast of a variable and a module that allows you to download the obtained data.")
+st.caption("The application has been enriched with a module for variable analysis, a module creating a forecast of a variable and a module that allows you to download the obtained data.")
 
 st.header('🔍 Variable Viewer')
 
