001e2a914f67f815a87f6be909c616bba88261f9
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-07 12:13:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -110,6 +110,7 @@ def plot():
           
           fig.update_layout(margin=dict(r=5, l=5, t=5, b=5))
           fig.update_yaxes(visible=False, showticklabels=False)
+          config = {'displayModeBar': False}
 
           fig.update_xaxes(rangeslider_visible=True)
                               
@@ -122,7 +123,7 @@ def plot():
                          dict(count=5, label="5y", step="year", stepmode="backward"),
                          dict(count=10, label="10y", step="year", stepmode="backward"),
                          dict(step="all")])))
-          st.plotly_chart(fig, use_container_width=True)
+          st.plotly_chart(fig, config=config, use_container_width=True)
                 
     with tab2:
      st.header("💾 Historical Data")
