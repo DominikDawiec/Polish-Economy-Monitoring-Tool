6888a54a14c19ff27f9586dbe8bf48d6a44a6b2d
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-07 09:44:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -491,11 +491,6 @@ st.info('Forecast build with SARIMA Model', icon="ℹ️")
 
 my_bar = st.progress(0)
 
-for percent_complete in range(100):
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1)
-     
-     
      
 if st.button('Say hello'):
           st.balloons()
