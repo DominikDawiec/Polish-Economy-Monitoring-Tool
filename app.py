a8a83c383845c877a68f464cefcb94f7b09720a6
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-09-07 12:05:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def plot():
                          dict(count=5, label="5y", step="year", stepmode="backward"),
                          dict(count=10, label="10y", step="year", stepmode="backward"),
                          dict(step="all")])))
-          st.plotly_chart(fig, config=config, use_container_width=True)
+          st.plotly_chart(fig, use_container_width=True)
                 
     with tab2:
      st.header("💾 Historical Data")
