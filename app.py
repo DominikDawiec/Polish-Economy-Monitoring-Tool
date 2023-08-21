4e98b88c808aa144a7fc026dd86e69f86cedcdbb
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2023-08-21 19:03:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -320,7 +320,6 @@ def analitical_insights():
                
         
 def forecast():
-     forecast.pred_ci = pred_ci
      st.subheader("🔮 Variable Forecast")
 
      def testStationarity(ts):
@@ -526,5 +525,4 @@ timeseries = timeseries.set_index(['date'])
 timeseries = timeseries.drop(columns=['values','percentage change','value difference'])
 
 forecast()
-pred_ci = forecast.pred_ci
 forecast_plot()
